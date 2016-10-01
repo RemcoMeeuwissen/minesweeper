e105bd2566a243280451f8dc4352aa6cd3bd7d7d
Dearon <dearon@gmail.com>
2016-10-01 22:48:33 +0200

    Refactored index.js for readability and such

--- routes/index.js
+++ routes/index.js
@@ -6,38 +6,49 @@ const minesweeper = require('../minesweeper');
 const router = express.Router();
 const db = new sqlite3.Database('sweeper.db');
 
-router.get('/', (req, res) => {
-  let user = '';
+const getUser = (req, res) => {
   if (req.cookies.user === undefined) {
-    user = uuid.v4();
+    const user = uuid.v4();
     res.cookie('user', user, { expires: new Date(Date.now() + (24 * (60 * (60 * 1000)))) });
-  } else {
-    user = req.cookies.user;
+    return user;
   }
+
+  return req.cookies.user;
+};
+
+const getField = (saveRow, user) => {
+  if (saveRow === undefined) {
+    const field = minesweeper.buildField(minesweeper.generateBombs());
+    db.run('INSERT INTO saves (userid, field) VALUES (?, ?)', user, JSON.stringify(field));
+    return field;
+  }
+
+  return JSON.parse(saveRow.field);
+};
+
+const getMessage = (gameStatus, user) => {
+  if (gameStatus.lost || gameStatus.won) {
+    db.run('DELETE FROM saves WHERE userid=?', user);
+
+    if (gameStatus.lost) {
+      return 'You\'ve lost :(';
+    } else if (gameStatus.won) {
+      return 'You\'ve won!';
+    }
+  }
+
+  return null;
+};
+
+router.get('/', (req, res) => {
+  const user = getUser(req, res);
 
   db.get('SELECT * FROM saves WHERE userid=?', user, (selectErr, row) => {
     if (selectErr) {
       res.render('error', { message: 'Database connection failed' });
     } else {
-      let field = '';
-
-      if (row === undefined) {
-        field = minesweeper.buildField(minesweeper.generateBombs());
-        db.run('INSERT INTO saves (userid, field) VALUES (?, ?)', user, JSON.stringify(field));
-      } else {
-        field = JSON.parse(row.field);
-      }
-
-      const gameStatus = minesweeper.checkStatus(field);
-      let message = null;
-
-      if (gameStatus.lost) {
-        db.run('DELETE FROM saves WHERE userid=?', user);
-        message = 'You\'ve lost :(';
-      } else if (gameStatus.won) {
-        db.run('DELETE FROM saves WHERE userid=?', user);
-        message = 'You\'ve won!';
-      }
+      const field = getField(row, user);
+      const message = getMessage(minesweeper.checkStatus(field), user);
 
       res.render('index', { field, message });
     }
@@ -45,14 +56,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-  let user = '';
-
-  if (req.cookies.user === undefined) {
-    user = uuid.v4();
-    res.cookie('user', user, { expires: new Date(Date.now() + (24 * (60 * (60 * 1000)))) });
-  } else {
-    user = req.cookies.user;
-  }
+  const user = getUser(req, res);
 
   db.get('SELECT * FROM saves WHERE userid=?', user, (selectErr, row) => {
     if (selectErr) {
@@ -75,14 +79,7 @@ router.get('/:coords', (req, res) => {
   const coords = req.params.coords;
   const x = Number(coords[0]);
   const y = Number(coords[2]);
-  let user = '';
-
-  if (req.cookies.user === undefined) {
-    user = uuid.v4();
-    res.cookie('user', user, { expires: new Date(Date.now() + (24 * (60 * (60 * 1000)))) });
-  } else {
-    user = req.cookies.user;
-  }
+  const user = getUser(req, res);
 
   db.get('SELECT * FROM saves WHERE userid=?', user, (selectErr, row) => {
     if (selectErr) {
@@ -90,8 +87,7 @@ router.get('/:coords', (req, res) => {
     } else if (row === undefined) {
       res.redirect('/');
     } else {
-      let field = JSON.parse(row.field);
-      field = minesweeper.revealTile(field, x, y);
+      const field = minesweeper.revealTile(getField(row, user), x, y);
 
       db.run('UPDATE saves SET field=? WHERE userid=?', JSON.stringify(field), user, (updateErr) => {
         if (updateErr) {
