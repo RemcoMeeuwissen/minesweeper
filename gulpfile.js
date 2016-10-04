00246d5db15b09304b35cb41c3da8ad8a97b685c
Dearon <dearon@gmail.com>
2016-10-05 00:02:35 +0200

    Cleaned up style.css and added a gulp task to prefix and minify the CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 gulp.task('javascript', () => browserify('./minesweeper/frontend.js')
   .transform('babelify', { presets: ['es2015'] })
@@ -9,4 +12,13 @@ gulp.task('javascript', () => browserify('./minesweeper/frontend.js')
   .pipe(source('minesweeper.min.js'))
   .pipe(gulp.dest('./public/js')));
 
-gulp.task('default', ['javascript']);
+gulp.task('css', () => gulp.src('./public/css/style.css')
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false,
+  }))
+  .pipe(cleanCSS())
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('./public/css')));
+
+gulp.task('default', ['javascript', 'css']);
